c4163d9f36fdfae7cd1ad83133a582328daf40f8
Sean Hagstrom <seanhagstrom1@gmail.com>
2021-10-25 16:12:49 -0400

    completed workshop

--- app.js
+++ app.js
@@ -6,6 +6,16 @@ const {
 } = require('./db');
 const path = require('path');
 
+const requireToken = async (req, res, next) => {
+  try {
+    req.user = await User.byToken(req.headers.authorization);
+    next();
+  } catch (error) {
+    console.error(error);
+    next(error);
+  }
+};
+
 app.get('/', (req, res) => res.sendFile(path.join(__dirname, 'index.html')));
 
 app.post('/api/auth', async (req, res, next) => {
@@ -16,19 +26,21 @@ app.post('/api/auth', async (req, res, next) => {
   }
 });
 
-app.get('/api/auth', async (req, res, next) => {
+app.get('/api/auth', requireToken, async (req, res, next) => {
   try {
-    res.send(await User.byToken(req.headers.authorization));
+    res.send(req.user);
   } catch (ex) {
     next(ex);
   }
 });
 
 // GET /api/notes/:userID **get all notes by userid**
-app.get('/api/notes/:userId', async (req, res, next) => {
+app.get('/api/users/notes', requireToken, async (req, res, next) => {
   try {
+    const user = req.user;
+
     const userNotes = await Note.findAll({
-      where: { userId: req.params.userId },
+      where: { userId: user.id },
     });
     res.json(userNotes);
   } catch (error) {
